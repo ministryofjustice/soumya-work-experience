bb64d1123588f168b5c8bff8a2fa1a3114133417
Matthew Price <matthew.price2@justice.gov.uk>
2025-04-15 14:10:46 +0100

    Add tests for the redact function (#3)
    * Add pytest to dev dependencies
    
    * Add tests for the redact function

---
+++ tests/test_pipeline.py
@@ -0,0 +1,54 @@
+"""Tests for the pipeline module."""
+from src.pipeline import (
+    redact,
+)
+
+
+class TestRedact:
+    """Tests for the redact function."""
+
+    def test_with_redaction(self):
+        """Test the expected functionality."""
+        row = {
+            "first_name": "John",
+            "last_name": "Smith",
+            "favourite_food": "rhubard",
+        }
+
+        columns_to_redact = ["first_name", "last_name"]
+
+        actual = redact(
+            row=row,
+            columns=columns_to_redact
+        )
+
+        expected = {
+            "first_name": "***",
+            "last_name": "***",
+            "favourite_food": "rhubard",
+        }
+
+        assert actual == expected
+
+    def test_no_redaction(self):
+        """Test the expected functionality."""
+        row = {
+            "first_name": "John",
+            "last_name": "Smith",
+            "favourite_food": "rhubard",
+        }
+
+        columns_to_redact = []
+
+        actual = redact(
+            row=row,
+            columns=columns_to_redact
+        )
+
+        expected = {
+            "first_name": "John",
+            "last_name": "Smith",
+            "favourite_food": "rhubard",
+        }
+
+        assert actual == expected
